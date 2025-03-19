f2cf20bc85c62881f8fd5af7f4f327431a4e9368
fmoorenoo <fermontemore@gmail.com>
2025-03-19 14:47:52 +0000

    Requisitos modificados para no permitir espacios en blanco

--- frontend/app/src/main/java/com/haircloud/utils/CredentialsValidator.kt
+++ frontend/app/src/main/java/com/haircloud/utils/CredentialsValidator.kt
@@ -5,23 +5,27 @@ object CredentialsValidator {
      - Al menos 4 letras
      - Al menos un número
      - Solo puede contener letras, números y los símbolos: - / . / _
+     - No puede contener espacios
     */
     fun isPasswordValid(password: String): Boolean {
         val letterCount = password.count { it.isLetter() }
         val hasNumber = password.any { it.isDigit() }
         val hasOnlyAllowedChars = password.all { it.isLetterOrDigit() || it in "-._" }
+        val hasNoSpaces = !password.contains(" ")
 
-        return letterCount >= 4 && hasNumber && hasOnlyAllowedChars
+        return letterCount >= 4 && hasNumber && hasOnlyAllowedChars && hasNoSpaces
     }
 
     /* Requisitos de nombre de usuario:
      - Mínimo 6 caracteres y máximo 20
      - Solo puede contener letras, números y los símbolos: . / _
+     - No puede contener espacios
     */
     fun isUsernameValid(username: String): Boolean {
         val isCorrectLength = username.length in 6..20
         val hasOnlyAllowedChars = username.all { it.isLetterOrDigit() || it in "._" }
+        val hasNoSpaces = !username.contains(" ")
 
-        return isCorrectLength && hasOnlyAllowedChars
+        return isCorrectLength && hasOnlyAllowedChars && hasNoSpaces
     }
 }
