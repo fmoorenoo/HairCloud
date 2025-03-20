afa58396dcb3169bb2b8cba43545b23df9ea9d0c
fmoorenoo <fermontemore@gmail.com>
2025-03-20 13:38:19 +0000

    Eliminados imports innecesarios

--- frontend/app/src/main/java/com/haircloud/MainActivity.kt
+++ frontend/app/src/main/java/com/haircloud/MainActivity.kt
@@ -4,15 +4,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.lifecycle.viewmodel.compose.viewModel
-import androidx.compose.foundation.layout.Box
 import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.haircloud.navigation.AppNavigation
-import com.haircloud.screens.LoginScreen
-import com.haircloud.screens.RegisterScreen
 import com.haircloud.viewmodel.UserViewModel
 
 class MainActivity : ComponentActivity() {
@@ -30,7 +25,7 @@ fun HairCloudApp(userViewModel: UserViewModel) {
     val navController = rememberNavController()
     var userRole by rememberSaveable { mutableStateOf<String?>(null) }
 
-    AppNavigation(navController, userRole, userViewModel)
+    AppNavigation(navController, userViewModel)
 
     LaunchedEffect(userRole) {
         userRole?.let {
