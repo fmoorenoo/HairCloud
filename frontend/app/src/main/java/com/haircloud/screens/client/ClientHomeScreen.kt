4bf96b2aec7232d5162c0d0abe8c3b1446eaa8a4
fmoorenoo <fermontemore@gmail.com>
2025-03-21 15:09:31 +0000

    Mostrar que el inicio de sesión fue exitoso

--- frontend/app/src/main/java/com/haircloud/screens/client/ClientHomeScreen.kt
+++ frontend/app/src/main/java/com/haircloud/screens/client/ClientHomeScreen.kt
@@ -10,13 +10,11 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
@@ -32,15 +30,26 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.haircloud.R
+import com.haircloud.utils.CustomSnackbarHost
+import com.haircloud.utils.SnackbarType
+import com.haircloud.utils.showTypedSnackbar
 
 @Composable
 fun ClientHomeScreen(navController: NavController) {
+    val snackbarHostState = remember { SnackbarHostState() }
 
     val blueWhiteGradient = Brush.verticalGradient(
         colors = listOf(Color(0xFF77AEE2), Color(0xFFFFFFFF))
     )
     val headersFont = FontFamily(Font(R.font.headers_font, FontWeight.Normal))
     val defaultFont = FontFamily(Font(R.font.default_font, FontWeight.Normal))
+
+    LaunchedEffect(key1 = true) {
+        snackbarHostState.showTypedSnackbar(
+            message = "Inicio de sesión exitoso",
+            type = SnackbarType.SUCCESS
+        )
+    }
 
     Box(
         modifier = Modifier
@@ -70,5 +79,14 @@ fun ClientHomeScreen(navController: NavController) {
                 )
             }
         }
+
+        // Snackbar
+        CustomSnackbarHost(
+            hostState = snackbarHostState,
+            modifier = Modifier
+                .align(Alignment.BottomCenter)
+                .padding(bottom = 30.dp),
+            defaultFont = defaultFont
+        )
     }
 }
