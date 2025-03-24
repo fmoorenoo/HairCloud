b99ab3255872057076238622d2d5572054f97633
fmoorenoo <fermontemore@gmail.com>
2025-03-24 14:22:44 +0000

    Barra de búsqueda y botón para filtrar

--- frontend/app/src/main/java/com/haircloud/screens/client/ClientHomeScreen.kt
+++ frontend/app/src/main/java/com/haircloud/screens/client/ClientHomeScreen.kt
@@ -17,12 +17,20 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Bookmarks
 import androidx.compose.material.icons.filled.CalendarMonth
+import androidx.compose.material.icons.filled.FilterList
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
@@ -45,6 +53,7 @@ import com.haircloud.utils.showTypedSnackbar
 @Composable
 fun ClientHomeScreen(navController: NavController) {
     val snackbarHostState = remember { SnackbarHostState() }
+    var searchQuery by remember { mutableStateOf("") }
 
     val blackWhiteGradient = Brush.verticalGradient(colors = listOf(Color(0xFF212121), Color(0xFF666F77)))
     val headersFont = FontFamily(Font(R.font.headers_font, FontWeight.Normal))
@@ -64,51 +73,106 @@ fun ClientHomeScreen(navController: NavController) {
             .padding(top = 16.dp)
     ) {
         Column(
-            modifier = Modifier.fillMaxSize(),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
         ) {
-            Column(
+            Row(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .padding(16.dp)
+                    .padding(horizontal = 8.dp),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceBetween
             ) {
-                Row(
+                Image(
+                    painter = painterResource(id = R.drawable.app_lightlogo),
+                    contentDescription = "Logo",
                     modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(horizontal = 8.dp),
-                    verticalAlignment = Alignment.CenterVertically,
-                    horizontalArrangement = Arrangement.SpaceBetween
-                ) {
-                    Image(
-                        painter = painterResource(id = R.drawable.app_lightlogo),
-                        contentDescription = "Logo",
-                        modifier = Modifier
-                            .height(50.dp)
-                            .wrapContentHeight(),
-                        contentScale = ContentScale.Inside
-                    )
-                    Image(
-                        painter = painterResource(id = R.drawable.user_profile_1),
-                        contentDescription = "Profile",
-                        modifier = Modifier
-                            .height(55.dp)
-                            .wrapContentHeight(),
-                        contentScale = ContentScale.Inside
-                    )
-                }
-                Spacer(modifier = Modifier.height(16.dp))
+                        .height(50.dp)
+                        .wrapContentHeight(),
+                    contentScale = ContentScale.Inside
+                )
+                Image(
+                    painter = painterResource(id = R.drawable.user_profile_1),
+                    contentDescription = "Profile",
+                    modifier = Modifier
+                        .height(55.dp)
+                        .wrapContentHeight(),
+                    contentScale = ContentScale.Inside
+                )
+            }
+            Spacer(modifier = Modifier.height(16.dp))
+            Row(horizontalArrangement = Arrangement.Center, modifier = Modifier.fillMaxWidth()) {
                 Text(
                     text = "Barberías",
                     color = Color.White,
                     style = TextStyle(fontFamily = defaultFont),
-                    fontSize = 40.sp,
+                    fontSize = 45.sp,
                     textAlign = TextAlign.Center,
                     fontWeight = FontWeight.Bold,
                     modifier = Modifier.padding(top = 8.dp)
                 )
             }
+
+            Spacer(modifier = Modifier.height(24.dp))
+
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 8.dp),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.spacedBy(12.dp)
+            ) {
+                OutlinedTextField(
+                    value = searchQuery,
+                    onValueChange = { searchQuery = it },
+                    modifier = Modifier
+                        .weight(1f),
+                    placeholder = { Text("Buscar por nombre", color = Color(0xFF4D4D4D),
+                        style = TextStyle(fontFamily = defaultFont), fontSize = 26.sp) },
+                    leadingIcon = {
+                        Icon(
+                            imageVector = Icons.Default.Search,
+                            contentDescription = "Search Icon",
+                            tint = Color(0xFF4D4D4D)
+                        )
+                    },
+                    shape = RoundedCornerShape(22.dp),
+                    colors = OutlinedTextFieldDefaults.colors(
+                        unfocusedContainerColor = Color(0xFFD9D9D9),
+                        focusedContainerColor = Color(0xFFD9D9D9),
+                        unfocusedBorderColor = Color.Transparent,
+                        focusedBorderColor = Color.Transparent,
+                        cursorColor = Color.DarkGray,
+                        unfocusedTextColor = Color.Black,
+                        focusedTextColor = Color.Black
+                    ),
+                    textStyle = TextStyle(
+                        fontFamily = defaultFont,
+                        fontSize = 26.sp
+                    ),
+                    singleLine = true
+                )
+
+                IconButton(
+                    onClick = {},
+                    modifier = Modifier
+                        .size(67.dp)
+                        .background(
+                            color = Color(0xFFD9D9D9),
+                            shape = RoundedCornerShape(22.dp)
+                        )
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.FilterList,
+                        contentDescription = "Filter",
+                        tint = Color(0xFF3B3B3B),
+                        modifier = Modifier.size(36.dp)
+                    )
+                }
+            }
         }
 
-        // Bottom menu - placed directly in the Box to ensure it's at the bottom
         Row(
             modifier = Modifier
                 .fillMaxWidth()
