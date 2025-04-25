04586a3486ff06eee8048412faca9b6543d3c0f6
fmoorenoo <fermontemore@gmail.com>
2025-04-25 14:24:14 +0100

    Variables para alternar la conexión entre el emulador y un dispositivo

--- frontend/app/src/main/java/com/haircloud/data/ApiClient.kt
+++ frontend/app/src/main/java/com/haircloud/data/ApiClient.kt
@@ -6,7 +6,10 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object ApiClient {
-    private const val BASE_URL = "http://10.0.2.2:5000/"
+    const val EMULATOR_URL = "http://10.0.2.2:5000/"
+    const val DEVICE_URL = "http://IP_DEVICE:5000/"
+    private const val BASE_URL = EMULATOR_URL
+
 
     // Registrar las peticiones y respuestas HTTP en Logcat
     private val loggingInterceptor = HttpLoggingInterceptor().apply {
