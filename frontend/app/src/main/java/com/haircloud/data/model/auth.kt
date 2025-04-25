e0eaa1979299379936f995df0c16a8533ed38f44
fmoorenoo <fermontemore@gmail.com>
2025-04-25 13:49:11 +0100

    Modelo modificado para aceptar el token generado

--- frontend/app/src/main/java/com/haircloud/data/model/auth.kt
+++ frontend/app/src/main/java/com/haircloud/data/model/auth.kt
@@ -1,7 +1,15 @@
 package com.haircloud.data.model
 
 data class LoginRequest(val nombreusuario: String, val password: String)
-data class LoginResponse(val message: String, val usuarioid: Int, val nombreusuario: String, val rol: String)
+
+data class LoginResponse(
+    val message: String,
+    val usuarioid: Int,
+    val nombreusuario: String,
+    val rol: String,
+    val token: String
+)
 
 data class RegisterRequest(val nombre_completo: String, val email: String, val nombreusuario: String, val password: String)
+
 data class RegisterResponse(val message: String, val usuarioid: Int)
