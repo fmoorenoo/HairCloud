40edb20ee5b760d9933f0706570ced6dbc0f61e7
fmoorenoo <fermontemore@gmail.com>
2025-04-25 14:28:38 +0100

    Variable para alternar la dirección del backend entre un emulador o un dispositivo real

--- backend/app.py
+++ backend/app.py
@@ -2,5 +2,10 @@ from app import create_app
 
 app = create_app()
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    using_real_device = False
+
+    if using_real_device:
+        app.run(host="0.0.0.0", port=5000, debug=True)
+    else:
+        app.run(debug=True)
