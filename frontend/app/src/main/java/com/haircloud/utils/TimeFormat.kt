b311d24570deeff2088363716e96c3e5a2e8a9aa
fmoorenoo <fermontemore@gmail.com>
2025-05-05 14:18:44 +0100

    Función formatTime() corregida y función formatDateToLong() creada

--- frontend/app/src/main/java/com/haircloud/utils/TimeFormat.kt
+++ frontend/app/src/main/java/com/haircloud/utils/TimeFormat.kt
@@ -1,20 +1,37 @@
 package com.haircloud.utils
 
-
+import java.time.LocalDate
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
+import java.time.format.TextStyle
+import java.util.Locale
 
-// Cadena de fecha y hora en formato ISO (2023-05-20T14:30:00) a formato de hora (14:30)
+// Convertir fecha a formato de hora (12:30)
 fun formatTime(dateTimeString: String): String {
     return try {
         val dateTime = LocalDateTime.parse(dateTimeString, DateTimeFormatter.ISO_DATE_TIME)
         dateTime.format(DateTimeFormatter.ofPattern("HH:mm"))
-    } catch (e: Exception) {
+    } catch (_: Exception) {
         try {
-            val dateTime = LocalDateTime.parse(dateTimeString)
+            val inputFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
+            val dateTime = LocalDateTime.parse(dateTimeString, inputFormatter)
             dateTime.format(DateTimeFormatter.ofPattern("HH:mm"))
-        } catch (e: Exception) {
-            dateTimeString
+        } catch (_: Exception) {
+            try {
+                val inputFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")
+                val dateTime = LocalDateTime.parse(dateTimeString, inputFormatter)
+                dateTime.format(DateTimeFormatter.ofPattern("HH:mm"))
+            } catch (_: Exception) {
+                dateTimeString
+            }
         }
     }
+}
+
+// Convertir fecha a formato largo (Lunes 20 de Mayo)
+fun formatDateToLong(date: LocalDate): String {
+    val dayOfWeek = date.dayOfWeek.getDisplayName(TextStyle.FULL, Locale("es", "ES"))
+    val dayOfMonth = date.dayOfMonth
+    val month = date.month.getDisplayName(TextStyle.FULL, Locale("es", "ES"))
+    return "${dayOfWeek.replaceFirstChar { it.uppercase() }} $dayOfMonth de ${month.lowercase()}"
 }
